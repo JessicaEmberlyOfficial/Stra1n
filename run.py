7e2add960c2c5cf8b5030c88042fff40c9b163aa
Jessica Emberly <jessicaemberly1+github@gmail.com>
2026-09-01 09:19:28 -0500

    Add files via upload

--- run.py
+++ run.py
@@ -1,9 +1,28 @@
 import os
+import time
 from encryptor import encrypt
-# from decryptor import decryptor
+from decryptor import decryptor
 os.system("clear")
 question = input("(d)ecrypt, or (e)ncrypt?: ")
 if question == "e":
   encrypt()
-if question == "d":
-  encrypt()
+elif question == "d":
+  decryptor()
+elif question != "e" or "d":
+  os.system("clear")
+  print("Invalid entry—exiting in [5].")
+  time.sleep(1)
+  os.system("clear")
+  print("Invalid entry—exiting in [4].")
+  time.sleep(1)
+  os.system("clear")
+  print("Invalid entry—exiting in [3].")
+  time.sleep(1)
+  os.system("clear")
+  print("Invalid entry—exiting in [2].")
+  time.sleep(1)
+  os.system("clear")
+  print("Invalid entry—exiting in [1].")
+  time.sleep(1)
+  os.system("clear")
+  os.system("exit")
