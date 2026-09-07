8635bc81c24e57ece50641ea7fe62536c3be5ba3
Jessica Emberly <jessicaemberly1+github@gmail.com>
2026-09-07 16:10:26 -0500

    Add files via upload

--- encryptor.py
+++ encryptor.py
@@ -172,47 +172,22 @@ def encrypt():
     # Replacing _password in order to obfuscate it.
     _new_password = _password.replace("a", a + " ").replace("b", b + " ").replace("c", c + " ").replace("d", d + " ").replace("e", e + " ").replace("f", f + " ").replace("g", g + " ").replace("h", h + " ").replace("i", i + " ").replace("j", j + " ").replace("k", k + " ").replace("l", l + " ").replace("m", m + " ").replace("n", n + " ").replace("o", o + " ").replace("p", p + " ").replace("q", q + " ").replace("r", r + " ").replace("s", s + " ").replace("t", t + " ").replace("u", u + " ").replace("v", v + " ").replace("w", w + " ").replace("x", x + " ").replace("y", y + " ").replace("z", z + " ").replace("A", a + ".7").replace("B", b + ".7").replace("C", c + ".7").replace("D", d + ".7").replace("E", e + ".7").replace("F", f + ".7").replace("G", g + ".7").replace("H", h + ".7").replace("I", i + ".7").replace("J", j + ".7").replace("K", k + ".7").replace("L", l + ".7").replace("M", m + ".7").replace("N", n + ".7").replace("O", o + ".7").replace("P", p + ".7").replace("Q", q + ".7").replace("R", r + ".7").replace("S", s + ".7").replace("T", t + ".7").replace("U", u + ".7").replace("V", v + ".7").replace("W", w + ".7").replace("X", x + ".7").replace("Y", y + ".7").replace("Z", z + ".7").replace("@", one).replace("#", two).replace("$", three).replace("&", four).replace("*", five).replace("-", six).replace("=", seven).replace("(", eight).replace(")", nine).replace("!", ten).replace('"', eleven).replace("'", twelve).replace(":", thirteen).replace(";", nine).replace("/", ten).replace("?", eleven).replace(",", twelve).replace("¡", thirteen).replace("<", fourteen).replace(">", fifteen).replace("¢", sixteen).replace("|", seventeen).replace("¿", eighteen).replace("©", nineteen).replace("®", twenty).replace("+", twenty_one).replace("±", twenty_two).replace("{", twenty_three).replace("}", twenty_four).replace("[", twenty_five).replace("]", twenty_seven).replace("~", twenty_eight).replace("÷", twenty_nine).replace("•", thirty).replace("°", thirty_one).replace("`", thirty_two).replace("´", thirty_four).replace("¥", thirty_five).replace("£", thirty_six).replace("€", thirty_seven).replace("1", "/999/").replace("2", "/888/").replace("3", "/777/").replace("4", "/666/").replace("5", "/555/").replace("6", "/444/").replace("7", "/333/").replace("8", "/222/").replace("9", "/111/")
     
-    # Splitting _new_password.
-    first_part = _new_password[:len(_new_password)//2]
-    second_part = _new_password[len(_new_password)//2]
-    third_part = _new_password[len(_new_password)//2]
-    fourth_part = _new_password[len(_new_password)//2]
-    fifth_part = _new_password[len(_new_password)//2:]
-    _password_split_1 = "/01/" + first_part + "/01/"
-    _password_split_2 = "/02/" + second_part + "/02/"
-    _password_split_3 = "/03/" + third_part + "/03/"
-    _password_split_4 = "/04/" + fourth_part + "/04/"
-    _password_split_5 = "/05/" + fifth_part + "/05/"
-    _password_id = random.randrange(500, 1500)
-    _new_password_id = str(_password_id).replace("0", pid["zero"]).replace("1", pid["one"]).replace("2", pid["two"]).replace("3", pid["three"]).replace("4", pid["four"]).replace("5", pid["five"]).replace("6", pid["six"]).replace("7", pid["seven"]).replace("8", pid["eight"]).replace("9", pid["nine"])
+    _password_id = str(random.randrange(500, 1500))
+    _new_password_id = _password_id.replace("0", pid["zero"]).replace("1", pid["one"]).replace("2", pid["two"]).replace("3", pid["three"]).replace("4", pid["four"]).replace("5", pid["five"]).replace("6", pid["six"]).replace("7", pid["seven"]).replace("8", pid["eight"]).replace("9", pid["nine"])
     os.system("clear")
     input("Please write down: " + str(_password_id) + " - Once saved, press enter.")
 
-    # Joining _password_split(s) to data segments after splitting.
-    d1 = str(data_seg_1).join(_password_split_1)
-    d2 = str(data_seg_2).join(_password_split_2)
-    d3 = str(data_seg_3).join(_password_split_3)
-    d4 = str(data_seg_4).join(_password_split_4)
-    d5 = str(data_seg_5).join(_password_split_5)
-    d6 = str(data_seg_6).join(_new_password_id)
+    # Data segments.
+    d1 = str(data_seg_1)
+    d2 = str(data_seg_2)
+    d3 = str(data_seg_3)
+    d4 = str(data_seg_4)
+    d5 = str(data_seg_5)
+    d6 = str(data_seg_6)
     
   elif duwap == False:
     _new_message = _ev.replace("1", message["one"]).replace("2", message["two"]).replace("3", message["three"]).replace("4", message["four"]).replace("5", message["five"]).replace("6", message["six"]).replace("7", message["seven"]).replace("8", message["eight"]).replace("9", message["nine"]).replace("a", message["a"]).replace("b", message["b"]).replace("c", message["c"]).replace("d", message["d"]).replace("e", message["e"]).replace("f", message["f"]).replace("g", message["g"]).replace("h", message["h"]).replace("i", message["i"]).replace("j", message["j"]).replace("k", message["l"]).replace("l", message["l"]).replace("m", message["m"]).replace("n", message["n"]).replace("o", message["o"]).replace("p", message["p"]).replace("q", message["q"]).replace("r", message["r"]).replace("s", message["s"]).replace("t", message["t"]).replace("u", message["u"]).replace("v", message["v"]).replace("w", message["w"]).replace("x", message["x"]).replace("y", message["y"]).replace("z", message["z"]).replace("A", message["_A"]).replace("B", message["_B"]).replace("C", message["_C"]).replace("D", message["_D"]).replace("E", message["_E"]).replace("F", message["_F"]).replace("G", message["_G"]).replace("H", message["_H"]).replace("I", message["_I"]).replace("J", message["_J"]).replace("K", message["_K"]).replace("L", message["_L"]).replace("M", message["_M"]).replace("N", message["_N"]).replace("O", message["_O"]).replace("P", message["_P"]).replace("Q", message["_Q"]).replace("R", message["_R"]).replace("S", message["_S"]).replace("T", message["_T"]).replace("U", message["_U"]).replace("V", message["_V"]).replace("W", message["_W"]).replace("X", message["_X"]).replace("Y", message["_Y"]).replace("Z", message["_Z"])
 
-  # Split _new_message into 5 parts.
-  sixth_part = _new_message[:len(_new_message)//2]
-  seventh_part = _new_message[len(_new_message)//2]
-  eighth_part = _new_message[len(_new_message)//2]
-  ninth_part = _new_message[len(_new_message)//2]
-  tenth_part = _new_message[len(_new_message)//2:]
-  _new_message_join_1 = " ------start------ " + sixth_part + " ------end------ "
-  _new_message_join_2 = " -------start------- " + seventh_part + " -------end------- "
-  _new_message_join_3 = " --------start-------- " + eighth_part + " --------end-------- "
-  _new_message_join_4 = " ---------start--------- " + ninth_part + " ---------end---------"
-  _new_message_join_5 = " ----------start---------- " + tenth_part + " ----------end----------"
-  
-  # d7 is the obfuscated message data.
-  d7 = str(data_seg_7).join(_new_message_join_1) + str(random.randbytes(50 + int(increase_values_by))) + numbers + _new_message_join_2 + str(random.randbytes(100 + int(increase_values_by))) + _new_message_join_3 + numbers + str(random.randbytes(150 + int(increase_values_by))) + _new_message_join_4 + numbers + str(random.randbytes(200 + int(increase_values_by))) + _new_message_join_5 + str(random.randbytes(250 + int(increase_values_by))) + numbers + str(random.randbytes(300 + int(increase_values_by))) + str(random.randbytes(350 + int(increase_values_by))) + str(random.randbytes(400 + int(increase_values_by))) + str(random.randbytes(450 + int(increase_values_by))) + str(random.randbytes(500 + int(increase_values_by)))
   if os.path.isfile(os.getcwd() + "/data.txt") == True:
     os.system("clear")
     dfep = input("An encrypted file already exists, do you want to replace it? (y) or (n): ")
@@ -220,9 +195,9 @@ def encrypt():
       os.system("clear")
       with open(os.getcwd() + "/data.txt", 'w') as file:
         if duwap == True:
-          file.write(d1 + d2 + d3 + d4 + d5 + d6 + d7)
+          file.write(_new_password + d1 + d2 + d3 + _new_message + d4 + d5 + d6 + _new_password_id)
         elif duwap == False:
-          file.write(d7)
+          file.write(_new_message)
           os.system("clear")
       if os.path.isfile(os.getcwd() + "/data.txt") == True:
             print("Encrypted file created at: " + os.getcwd() + "/data.txt")
@@ -252,10 +227,10 @@ def encrypt():
       elif "." not in name:
         with open(os.getcwd() + name + ".", 'w') as file:
           if duwap == True:
-            file.write(d1 + d2 + d3 + d4 + d5 + d6 + d7)
+            file.write(_new_password + d1 + d2 + d3 + _new_message + d4 + d5 + d6 + _new_password_id)
             os.system("clear")
           if duwap == False:
-            file.write(d7)
+            file.write(_new_message)
             os.system("clear")
           if os.path.isfile(os.getcwd() + "/" + name + ".txt") == True:
             print("Encrypted file created at: " + os.getcwd() + "/" + name + ".txt")
@@ -272,9 +247,9 @@ def encrypt():
   elif os.path.isfile(os.getcwd() + "/data.txt") == False:
     with open(os.getcwd() + "/data.txt", 'w') as file:
       if duwap == True:
-        file.write(d1 + d2 + d3 + d4 + d5 + d6 + d7)
+        file.write(_new_password + d1 + d2 + d3 + _new_message + d4 + d5 + d6 + _new_password_id)
       else:
-        file.write(d7)
+        file.write(_new_message)
       os.system("clear")
       if os.path.isfile(os.getcwd() + "/data.txt") == True:
         print("Encrypted file created at: " + os.getcwd() + "/data.txt")
